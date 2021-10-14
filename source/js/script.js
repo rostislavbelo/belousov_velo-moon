b13675aeeb12a665dacc91621fb693bcdad6ef38
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2021-10-14 17:08:16 +0300

    Вносит правки по замечаниям

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,11 @@
-const startMobileMenu = function () {
+(function () {
   const buttonMenu = document.querySelector(".mobile-menu__button");
   const menuActivate = document.querySelector(".mobile-menu__items");
   const hidden = document.querySelectorAll(".mobile-hidden");
   const itemMenu = document.querySelectorAll(".main-nav__item-content")
 
 
-  window.onload = function () {
+  if (buttonMenu && menuActivate) {
     buttonMenu.classList.remove("mobile-menu__button--active");
     menuActivate.classList.remove("mobile-menu__items--active");
     buttonMenu.classList.remove("mobile-menu__button--hidden")
@@ -15,21 +15,23 @@ const startMobileMenu = function () {
       menuActivate.classList.toggle("mobile-menu__items--active");
       buttonMenu.classList.toggle("mobile-menu__button--active");
 
-      hidden.forEach(function (el) {
-        el.classList.toggle("visually-hidden");
-      });
+      if (hidden) {
+        hidden.forEach(function (el) {
+          el.classList.toggle("visually-hidden");
+        });
+      };
     })
+
+    itemMenu.forEach(function (item) {
+      item.addEventListener("click", function () {
+        if (hidden) {
+          hidden.forEach(function (el) {
+            el.classList.remove("visually-hidden");
+          });
+        };
+        buttonMenu.classList.remove("mobile-menu__button--active");
+        menuActivate.classList.remove("mobile-menu__items--active");
+      })
+    });
   };
-
-  itemMenu.forEach(function (item) {
-    item.addEventListener("click", function () {
-      hidden.forEach(function (el) {
-        el.classList.remove("visually-hidden");
-      });
-
-      buttonMenu.classList.remove("mobile-menu__button--active");
-      menuActivate.classList.remove("mobile-menu__items--active");
-    })
-  });
-}
-startMobileMenu();
+})();
